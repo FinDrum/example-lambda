373b5cd731da1f1f5d6dc551a177e4a1f0e1686a
Óscar Rico Rodríguez <95436834+orr21@users.noreply.github.com>
2025-06-06 15:37:08 +0100

    v 1.0.0
    * Add infrastructure
    
    * update .gitignore
    
    * add yfinance-kafka-trigger
    
    * add config
    
    * add grafana
    
    * add postgres_writer.py
    
    * add real_time_pipeline
    
    * add requirements.txt
    
    * update readme
    
    * add grafana dashboard

---
+++ infrastructure/init/init.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS realtime_stock (
+  symbol TEXT,
+  timestamp TEXT,
+  open DOUBLE PRECISION,
+  close DOUBLE PRECISION,
+  volume BIGINT
+);
+
+CREATE TABLE IF NOT EXISTS hourly_stock_summary (
+    symbol TEXT,
+    hour TIMESTAMP,
+    avg_close DOUBLE PRECISION,
+    avg_volume DOUBLE PRECISION
+);
